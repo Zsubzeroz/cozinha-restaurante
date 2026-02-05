da7d478d0ab3ee3afbe43762cc75194987c69164
LUAN ESTIFER RODRIGUES PEREIRA <luanestiferjob@gmail.com>
2026-02-05 07:56:40 -0300

    feat: Implement DishType creation view and correct URL reference

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,16 +4,11 @@ from . import views
 app_name = 'restaurant'
 
 urlpatterns = [
-    path('criar_restaurant/', views.Criar_Restaurant, name='criar_restaurant'),
-    # DishType Views
     path('types/', views.DishTypeList.as_view(), name='dishtype_list'),
     path('types/<int:pk>/', views.DishTypeDetail.as_view(), name='dishtype_detail'),
-
-    # Dish Views
     path('dishes/', views.DishList.as_view(), name='dish_list'),
     path('dishes/<int:pk>/', views.DishDetail.as_view(), name='dish_detail'),
-
-    # Cook Views
     path('cooks/', views.CookList.as_view(), name='cook_list'),
     path('cooks/<int:pk>/', views.CookDetail.as_view(), name='cook_detail'),
+    path('types/new/', views.DishTypeCreateView.as_view(), name='dishtype_create'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
-from .models import Dish, DishType, Cook
+from .models import DishType, Dish, Cook
 
 
 
