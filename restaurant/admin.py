8ff04da200561fb3107fc36b5e7d230ebddc8ddb
LUAN ESTIFER RODRIGUES PEREIRA <luanestiferjob@gmail.com>
2026-02-22 20:22:56 -0300

    Refatoração: Adiciona through model DishIngredient com quantidade
    Signed-off-by: LUAN ESTIFER RODRIGUES PEREIRA <luanestiferjob@gmail.com>

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Dish, DishType, Cook, Ingredient, DishIngredient
 
-# Register your models here.
+
+class DishIngredientInline(admin.TabularInline):
+    model = DishIngredient
+    extra = 1
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "dish_type")
+    search_fields = ("name",)
+    inlines = [DishIngredientInline]
+
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = ("username", "first_name", "last_name", "email", "years_of_experience")
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional Info"), {"fields": ("years_of_experience",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (("Additional Info"), {"fields": ("years_of_experience",)}),
+    )
+
+
+admin.site.register(DishType)
+admin.site.register(Ingredient)
+admin.site.register(DishIngredient)
